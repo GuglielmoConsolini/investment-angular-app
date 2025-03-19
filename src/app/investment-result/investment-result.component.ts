8acb8fb2728edb522aceabc28752faae43dfedbc
Guglielmo Consolini <g.consolini@deeplab.it>
2025-03-19 14:13:22 +0100

    migration to modules

--- src/app/investment-result/investment-result.component.ts
+++ src/app/investment-result/investment-result.component.ts
@@ -1,11 +1,9 @@
-import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-result',
-  standalone: true,
-  imports: [CurrencyPipe],
+  standalone: false,
   templateUrl: './investment-result.component.html',
   styleUrl: './investment-result.component.css'
 })
