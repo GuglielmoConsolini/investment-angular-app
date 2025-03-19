ed20e316499cfd9118c9576ab66da0caf33fe3f4
Guglielmo Consolini <g.consolini@deeplab.it>
2025-03-19 11:54:48 +0100

    first commit

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input-user.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
+  initialInvestment = '0'
+  annualInvestment = '0'
+  expectedReturn = '5'
+  duration = '10'
+
+  onSubmit() {
+     this.calculate.emit({
+      initialInvestment: +this.initialInvestment,
+      annualInvestment: +this.annualInvestment,
+      expectedReturn: +this.expectedReturn,
+      duration: +this.duration
+     });
+  }
+
+}
